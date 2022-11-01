b36db9b1d2f8e176969bb0e8b879d0442e1ca71f
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2022-10-31 23:44:18 -0300

    Requisito criar um header para a carteira, ainda não descobri o erro que o requisito 1 dando erro apesar de estar funcionando

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userEmail } = this.props;
+    return (
+      <div>
+        <header>
+          <h4 data-testid="email-field">
+            {`E-mail: ${userEmail}`}
+          </h4>
+          <h3 data-testid="total-field">
+            0
+          </h3>
+          <h3 data-testid="header-currency-field">
+            BRL
+          </h3>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userEmail: state.userInfo.user.email,
+});
+
+Wallet.propTypes = {
+  // dispatch: PropTypes.func.isRequired,
+  userEmail: PropTypes.objectOf(PropTypes.func).isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
