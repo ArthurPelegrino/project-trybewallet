1a211521e0a4f26b5df9b8ac02998a5b90ca9ae1
Arthur Pelegrino <arthurpelegrino97@gmail.com>
2022-11-07 21:13:26 -0300

    Requisito 7, alimentando a tabela com os dados do meu estado global

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../pages/pagescss/Login.css';
+import PropTypes from 'prop-types';
+
+const mapStateToProps = (state) => ({
+  description: state.wallet.expenses,
+});
 
 class Table extends Component {
   render() {
+    // mapStateToProps()
+    const { description } = this.props;
+    // console.log('description', description);
     return (
-      <div>
-        <th>Descrição</th>
-        <th>Tag</th>
-        <th>Método de pagamento</th>
-        <th>Valor</th>
-        <th>Moeda</th>
-        <th>Câmbio utilizado</th>
-        <th>Valor convertido</th>
-        <th>Moeda de conversão</th>
-        <th>Editar/Excluir</th>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
 
-      </div>
+        <tbody>
+          {
+            description.map((info) => (
+              <tr key={ info.id }>
+                <td>{info.description}</td>
+                <td>{info.tag}</td>
+                <td>{info.method}</td>
+                <td>{Number(info.value).toFixed(2)}</td>
+                <td>{info.exchangeRates[info.currency].name}</td>
+                <td>{Number(info.exchangeRates[info.currency].ask).toFixed(2)}</td>
+                <td>
+                  {
+                    (Number(info.value) * info.exchangeRates[info.currency].ask)
+                      .toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+                <td>
+                  <button type="button" data-testid="delete-btn">Excluir</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  description: PropTypes.arrayOf(PropTypes.string).isRequired,
+  map: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
